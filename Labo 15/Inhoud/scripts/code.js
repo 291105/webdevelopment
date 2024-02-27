4165f1634cc526aa2889093843e5a66bacbee08c
291105 <r0993052@student.vives.be>
2024-02-27 15:24:27 +0100

    web

--- Labo 15/Inhoud/scripts/code.js
+++ Labo 15/Inhoud/scripts/code.js
@@ -1,4 +1,7 @@
 const setup = () => {
-    document.getElementsByClassName(belangrijk);
+    const neemKlasseBelangrijk= document.getElementsByClassName("belangrijk")
+    for(let i=0; i<neemKlasseBelangrijk.length; i++){
+        neemKlasseBelangrijk[i].classList.add("opvallend");
+    }
 }
 window.addEventListener("load", setup); 
