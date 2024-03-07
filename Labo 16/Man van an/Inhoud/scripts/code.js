0ea772dd12ffebabac7feda8199ce5a46349bec2
291105 <r0993052@student.vives.be>
2024-03-07 20:00:23 +0100

    web

--- Labo 16/Man van an/Inhoud/scripts/code.js
+++ Labo 16/Man van an/Inhoud/scripts/code.js
@@ -1,3 +1,15 @@
 const setup = () => {
+    let button= document.getElementById("tel");
+    button.addEventListener("click", vindAn)
+}
+const vindAn = ()=>{
+    let tekst= document.getElementById("tekst").textContent.toLowerCase();
+    let i=0;
+    let aantalVoorkomens;
+    while (i= tekst.indexOf("an", i)!==-1){
+        aantalVoorkomens++;
+        i+= 2;
+    }
+    console.log(aantalVoorkomens);
 }
 window.addEventListener("load", setup); 
