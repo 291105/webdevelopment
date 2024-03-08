f8fa38a38df29fb2ec9dd6161d62190e0925f6e5
291105 <r0993052@student.vives.be>
2024-03-08 17:03:33 +0100

    web

--- Labo 16/Man van an/Inhoud/scripts/code.js
+++ Labo 16/Man van an/Inhoud/scripts/code.js
@@ -1,15 +1,38 @@
 const setup = () => {
-    let button= document.getElementById("tel");
-    button.addEventListener("click", vindAn)
+    let button= document.getElementById("indexOf");
+    button.addEventListener("click", vindAnMetIndexOf)
+    let button2= document.getElementById("lastIndexOf")
+    button2.addEventListener("click", vindAnMetLastIndexOf)
 }
-const vindAn = ()=>{
+const vindAnMetIndexOf = ()=>{
     let tekst= document.getElementById("tekst").textContent.toLowerCase();
     let i=0;
-    let aantalVoorkomens;
-    while (i= tekst.indexOf("an", i)!==-1){
-        aantalVoorkomens++;
-        i+= 2;
+    let aantalVoorkomens=0;
+    let stoppen= false;
+    while(stoppen===false){
+        if(tekst.indexOf("an", i)!==-1){
+            i= tekst.indexOf("an", i)+ 1;
+            aantalVoorkomens++;
+        }else {
+            stoppen= true;
+        }
     }
-    console.log(aantalVoorkomens);
+    console.log("Aantal keer:" + aantalVoorkomens);
+}
+
+const vindAnMetLastIndexOf= () =>{
+    let tekst= document.getElementById("tekst").textContent.toLowerCase();
+    let i= tekst.length;
+    let aantalVoorkomens=0;
+    let stoppen= false;
+    while(stoppen===false){
+        if(tekst.lastIndexOf("an", i)!==-1){
+                i= tekst.lastIndexOf("an", i) -1;
+                aantalVoorkomens++;
+        }else {
+            stoppen= true;
+        }
+    }
+    console.log("Aantal keer:" + aantalVoorkomens);
 }
 window.addEventListener("load", setup); 
